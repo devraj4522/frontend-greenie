0514007ab03381ca0ae71b88a1c5c13ced01e334
Dev Raj Singh <rehansingh.4522@gmail.com>
2023-02-13 00:49:28 +0530

    CartImage

--- src/Components/CheckoutCard/ShowCartImage.jsx
+++ src/Components/CheckoutCard/ShowCartImage.jsx
@@ -10,7 +10,7 @@ export const ShowCartImage = (props) => {
         image = imageList[0].public_id;
         console.log(image)
     }
-
+    
     console.log(image)
     if (!image) {
         image = 'https://images.unsplash.com/photo-1621076806681-a82129e952d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
