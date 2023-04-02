710b372be30df78e7a325837d391ca003c03e3bd
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2023-04-02 21:12:56 +0530

    Update backend.js

--- src/backend.js
+++ src/backend.js
@@ -1,4 +1,5 @@
-export const API = process.env.REACT_APP_BACKEND;
+// export const API = process.env.REACT_APP_BACKEND;
+export const API = process.env.REACT_APP_BACKEND2
 export const MEDIA_CLOUD_NAME = 'dhcwfa4vu';
 
 // export const API = "http://localhost:8000";
